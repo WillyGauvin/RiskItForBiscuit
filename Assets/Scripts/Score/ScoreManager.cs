756bc1cf4ff4f1247b10d1abb3c462be93547fdc
JStambulic <144737735+JStambulic@users.noreply.github.com>
2025-08-25 11:58:44 +0200

    Organized and added debt accumulation in case of negative funds

--- Assets/Scripts/Score/ScoreManager.cs
+++ Assets/Scripts/Score/ScoreManager.cs
@@ -5,9 +5,11 @@ public class ScoreManager : MonoBehaviour
     public static ScoreManager instance { get; private set; }
 
     public uint currentScore { get; private set; }
+    // Allows for score and money to not be 1:1. Huge score = dopamine, but not infinite money.
     [SerializeField, Range(0, 1)] float scoreMoneyConversion = 0.1f;
 
-    public int currentMoney { get; private set; }
+    public float currentMoney { get; private set; }
+    public float debtInterest {  get; private set; }
 
     private void Awake()
     {
@@ -19,7 +21,10 @@ public class ScoreManager : MonoBehaviour
 
         ResetScore();
         ResetMoney();
+        SetDebtInterest();
     }
+
+    #region Score and Value Resets
 
     /// <summary>
     /// Reset current score back to default.
@@ -34,17 +39,31 @@ public class ScoreManager : MonoBehaviour
     /// </summary>
     public void ResetMoney()
     {
-        currentMoney = 0;
+        currentMoney = 0.0f;
+    }
+
+    /// <summary>
+    /// Set the debt interest.
+    /// Can be changed day-to-day if necessary.
+    /// </summary>
+    /// <param name="interest">Interest to apply to negative funds.</param>
+    public void SetDebtInterest(float interest = 1.13f)
+    {
+        debtInterest = interest;
     }
 
     /// <summary>
     /// Add a value into score.
     /// </summary>
-    /// <param name="value">Unsigned integer to ensure score cannot be negative.</param>
+    /// <param name="value">Score to add. Unsigned integer to ensure score cannot be negative.</param>
     public void AddToScore(uint value)
     {
         currentScore += value;
     }
+
+    #endregion
+
+    #region Money
 
     /// <summary>
     /// Converts earned score into money to spend using a conversion value.
@@ -53,7 +72,7 @@ public class ScoreManager : MonoBehaviour
     {
         if (currentScore <= 0) { return; }
 
-        currentMoney = (int)(currentScore * scoreMoneyConversion);
+        currentMoney = currentScore * scoreMoneyConversion;
 
         ResetScore();
     }
@@ -66,6 +85,19 @@ public class ScoreManager : MonoBehaviour
     {
         if (price <= 0) { return; }
 
-        currentMoney -= (int)price;
+        currentMoney -= price;
     }
+
+    /// <summary>
+    /// Accumulate a player debt by interest rate.
+    /// To be applied at the end of every day if money is in the negatives.
+    /// </summary>
+    public void AccumulateDebt()
+    {
+        if (currentMoney >= 0) { return; }
+
+        currentMoney *= debtInterest;
+    }
+
+    #endregion
 }
