841974b097925a0803381d21b7832a4992d5792d
JStambulic <144737735+JStambulic@users.noreply.github.com>
2025-08-25 20:30:28 +0200

    Added day manager

---
+++ Assets/Scripts/DayManager.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class DayManager : MonoBehaviour
+{
+    public static DayManager instance;
+
+    public uint currentDay {get; private set;}
+    [SerializeField] int totalDays = 25;
+
+    public uint numDivesRemaining { get; private set; }
+    const int numDivesPerDay = 3;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one Day Night Manager in the scene.");
+        }
+        instance = this;
+
+        currentDay = 0;
+    }
+
+    private void Start()
+    {
+        StartNewDay();
+    }
+
+    /// <summary>
+    /// Begins a new day, increasing day counter and resetting the number of dives available.
+    /// </summary>
+    public void StartNewDay()
+    {
+        currentDay++;
+
+        if (currentDay >= totalDays)
+        {
+            // Debt was not paid on time. Game Over.
+        }
+
+        numDivesRemaining = numDivesPerDay;
+    }
+
+    /// <summary>
+    /// Reduces dives remaining. 
+    /// If no more dives remain, end the day.
+    /// </summary>
+    public void DivePerformed()
+    {
+        numDivesRemaining--;
+
+        if (numDivesRemaining <= 0)
+        {
+            EndCurrentDay();
+        }
+    }
+
+    /// <summary>
+    /// Ends the current day, turning accumulated score into money for the next day.
+    /// </summary>
+    void EndCurrentDay()
+    {
+        ScoreManager.instance.ConvertScoreToMoney();
+    }
+}
