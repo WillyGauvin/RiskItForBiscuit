f75343da500ca396da87db8ba230faac4cbbc6fd
gregorymolot <molo0037@algonquinlive.com>
2025-08-25 17:06:50 -0400

    Added dog ragdoll with controls

---
+++ Assets/Scripts/DogMovement/HingeSetter.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class HingeSetter : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    HingeJoint2D hinge;
+
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint2D>();
+    }
+
+    public void SetSpeed(float speed)
+    {
+        JointMotor2D motor = hinge.motor;
+        motor.motorSpeed = speed;
+        hinge.motor = motor;
+        hinge.useMotor = true;
+    }
+
+    public float GetAngle()
+    {
+        return hinge.jointAngle;
+    }
+
+    public float GetMaxAngle()
+    {
+        return hinge.limits.max;
+    }
+
+    public void ResetHinge()
+    {
+        hinge.useMotor = false;
+        // JointMotor2D motor = hinge.motor;
+        // motor.motorSpeed = speed *2f;
+        // hinge.motor = motor;
+    }
+
+    private void Update()
+    {
+
+        // if (resetHinge)
+        // {
+        //     if (hinge.jointAngle <= 1f)
+        //     {
+        //         JointMotor2D motor = hinge.motor;
+        //         motor.motorSpeed = 0;
+        //         hinge.motor = motor;
+        //         resetHinge = false;
+        //     }
+        // }
+
+    }
+}
