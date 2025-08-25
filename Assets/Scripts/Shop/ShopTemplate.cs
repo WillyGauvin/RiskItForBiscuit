adeeee237051f131cdadc89c738572896f59ad6c
Izaak Aiddi <izaakaidid613@gmail.com>
2025-08-25 18:30:12 -0400

    finished shop system, need to make the shop attribute values to the dock system

---
+++ Assets/Scripts/Shop/ShopTemplate.cs
@@ -0,0 +1,56 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShopTemplate : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private ShopUI shopUIRef;
+    [SerializeField] public string ShopName;
+    [SerializeField] public List<ItemTemplate> listOfItems;
+
+    void Start()
+    {
+        //find the shop ui in the scene
+        shopUIRef = FindAnyObjectByType<ShopUI>();
+
+        if (shopUIRef == null)
+        {
+            Debug.LogError("Add shopUI to Scene");
+        }
+
+        //add this shop to the shopUI's list of shops
+        shopUIRef.listOfItems.Add(this);
+    }
+
+    //open the shop UI using this shop's data
+    public void OpenShop()
+    {
+        
+        if (shopUIRef != null)
+        {
+            shopUIRef.gameObject.SetActive(true); //  Show UI
+            shopUIRef.SetCurrentShop(this);
+        }
+    }
+
+    //Close the shop UI
+    public void CloseShop()
+    {
+        if (shopUIRef != null)
+        {
+            shopUIRef.CloseShop();
+        }
+    }
+
+    //ull never guess what "OnClick" does
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //if the shop is clicked while its menu is open, close shop
+        if (shopUIRef.activeShop == this)
+            CloseShop();
+        //  Opens the shop when clicked
+        else
+            OpenShop(); 
+    }
+}
---
+++ Assets/Scripts/Shop/ShopUI.cs
@@ -0,0 +1,96 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopUI : MonoBehaviour
+{
+    [SerializeField] public List<ShopTemplate> listOfItems;   // All shops
+    [SerializeField] public ShopTemplate activeShop;          // Currently open shop
+    [SerializeField] public GameObject itemListUI;            // Panel with GridLayoutGroup
+    [SerializeField] private GameObject itemCardPrefab;       // Prefab for UI item (drag in Inspector)
+    [SerializeField] private TextMeshProUGUI shopNameRef;     // Name of the shop
+
+    void Start()
+    {
+        gameObject.SetActive(false); // hide by default
+    }
+
+    //Called when a new shop is selected/clicked to be the current shop
+    public void SetCurrentShop(ShopTemplate item)
+    {
+        if (item != activeShop)
+        {
+            //Close previous ui
+            CloseShop();
+            activeShop = item;
+
+            //Make UI visible
+            gameObject.SetActive(true);
+            //Set The Shops Name
+            shopNameRef.text = item.ShopName;
+            //Refresh the items shown
+            UpdateShop();
+        }
+    }
+
+    public void CloseShop()
+    {
+        activeShop = null;
+        gameObject.SetActive(false); // hide UI
+    }
+
+    public void UpdateShop()
+    {
+        // Basic safety checks
+        if (itemListUI == null)
+        {
+            Debug.LogError($"ShopUI ({name}): itemListUI is not assigned.");
+            return;
+        }
+
+        // Prevent accidentally clearing the shop root itself
+        if (itemListUI == this.gameObject)
+        {
+            Debug.LogError($"ShopUI ({name}): itemListUI points to the ShopUI root. Assign the child 'itemList' container instead. Aborting Clear.");
+            return;
+        }
+
+        Transform container = itemListUI.transform;
+
+        // Destroy direct children safely (iterate backwards to prevent error)
+        for (int i = container.childCount - 1; i >= 0; i--)
+        {
+            //find child of layout object
+            Transform child = container.GetChild(i);
+
+            //kill it :)
+            Destroy(child.gameObject); 
+        }
+
+        // If no active shop, nothing more to do
+        if (activeShop == null) return;
+
+        // Populate with items from the active shop
+        foreach (ItemTemplate item in activeShop.listOfItems)
+        {
+            GameObject card = Instantiate(itemCardPrefab, container, false); // 'false' keeps local transform
+            ItemTemplate cardTemplate = card.GetComponent<ItemTemplate>();
+            if (cardTemplate == null)
+            {
+                Debug.LogWarning("Item card prefab missing ItemTemplate component.");
+                continue;
+            }
+
+            //since C# sucks and thers no pointers, heres my solution updating the shops true values with the new child copy created for the UI
+            cardTemplate.BindTo(item); 
+            //update the card's name
+            card.name = $"Card - {item.GetItemName()}";
+        }
+
+        // Make layout update immediately so GridLayoutGroup arranges the new children now
+        RectTransform rt = container as RectTransform;
+        if (rt != null) LayoutRebuilder.ForceRebuildLayoutImmediate(rt);
+    }
+
+}
