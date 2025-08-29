914961a751fffe8d48a62a5b2d3992cb3a604b89
Will <williamgauvin6098@gmail.com>
2025-08-29 19:00:27 -0400

    Added obstacle props

--- Assets/Scripts/ObstaclePlacement/PreviewSystem.cs
+++ Assets/Scripts/ObstaclePlacement/PreviewSystem.cs
@@ -60,7 +60,7 @@ public class PreviewSystem : MonoBehaviour
     {
         if (size.x > 0 || size.y > 0)
         {
-            cellIndicator.transform.localScale = new Vector3(size.x, 1, size.y);
+            cellIndicator.transform.localScale = new Vector3(size.x * 5, 1, size.y * 5);
             cellIndicatorRenderer.material.mainTextureScale = size;
         }
     }
